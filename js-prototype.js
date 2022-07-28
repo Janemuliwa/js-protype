af0818ca6c7056c8d1cfbac209f6f849a60cbe4f
jane muliwa <muliwajane911@gmail.com>
2022-07-28 12:11:01 +0300

    prototype

---
+++ js-prototype.js
@@ -0,0 +1,56 @@
+Account.prototype.deposit = function(amount) {
+    if (this._isPositive(amount)) {
+      this.balance += amount;
+      console.info(`Deposit: ${this.name} new balance is ${this.balance}`);
+      return true;
+    }
+    return false;
+  }
+  
+  Account.prototype.withdraw = function(amount) {
+    if (this._isAllowed(amount)) {
+      this.balance -= amount;
+      console.info(`Withdraw: ${this.name} new balance is ${this.balance}`);
+      return true;
+    }
+    return false;
+  }
+  
+  Account.prototype.transfer = function(amount, account) {
+    if (this.withdraw(amount) && account.deposit(amount)) {
+      console.info(`Transfer: ${amount} has been moved from ${this.name} to ${account.name}`);
+      return true;
+    }
+    return false;
+  }
+  
+  
+  Account.prototype._isPositive = function(amount) {
+    const isPositive = amount > 0;
+    if (!isPositive) {
+      console.error('Amount must be positive!');
+      return false;
+    }
+    return true;
+  }
+  
+  Account.prototype._isAllowed = function(amount) {
+    if (!this._isPositive(amount)) return false;
+  
+    const isAllowed = this.balance - amount >= 0;
+    if (!isAllowed) {
+      console.error('You have insufficient funds!');
+      return false;
+    }
+    return true;
+  }
+  
+  const e = new Account('e', 100);
+  const f = new Account('f', 0);
+  
+  
+  output.innerText += `before:  e: ${e.balance}, f: ${f.balance}`;
+  
+  e.transfer(100, f);
+  
+  output.innerText += `after:  e: ${e.balance}, f: ${f.balance}`;
